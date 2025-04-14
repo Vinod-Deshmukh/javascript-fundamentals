66962d03d85fbb3f4a354716f381955a18058b50
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-04-14 12:02:35 +0530

    type conversion compelete

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Numeric conversion in mathematical functions and expressions happens automatically.
-alert("6"/"2")
+// alert("6"/"2")
+console.log(Boolean(9))
+console.log(Boolean(' '))
+console.log(typeof(undefined))
