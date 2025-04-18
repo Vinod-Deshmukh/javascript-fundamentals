ccacbc575d70e3723f837ec522f0f80f2d1a2f4a
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-04-18 13:21:54 +0530

    increment/decrement operators

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ let apples = "2";
 let oranges = "3";
 
 // both values converted to numbers before the binary plus
-alert( +apples + +oranges ); // 5
+// alert( +apples + +oranges ); // 5
 
 // the longer variant
 // alert( Number(apples) + Number(oranges) ); // 5
+
+let counter = 0;
+counter=++counter;
+alert(counter);
+alert(counter+10);
+
